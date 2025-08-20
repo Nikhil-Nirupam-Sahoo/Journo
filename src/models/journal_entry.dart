833bdbdb796baee77757c4c4c2b207f1f2327d11
Nikhil-Nirupam-Sahoo <nikhilnirupamsahoo@gmail.com>
2025-08-20 19:17:52 +0530

    Create journal_entry.dart

---
+++ src/models/journal_entry.dart
@@ -0,0 +1,35 @@
+import 'package:intl/intl.dart';
+
+class JournalEntry {
+  final String id;
+  String title;
+  String body;
+  DateTime createdAt;
+  DateTime updatedAt;
+
+  JournalEntry({
+    required this.id,
+    required this.title,
+    required this.body,
+    required this.createdAt,
+    required this.updatedAt,
+  });
+
+  String get formattedDate => DateFormat.yMMMEd().add_jm().format(updatedAt);
+
+  Map<String, dynamic> toJson() => {
+        'id': id,
+        'title': title,
+        'body': body,
+        'createdAt': createdAt.toIso8601String(),
+        'updatedAt': updatedAt.toIso8601String(),
+      };
+
+  factory JournalEntry.fromJson(Map<String, dynamic> json) => JournalEntry(
+        id: json['id'] as String,
+        title: json['title'] as String,
+        body: json['body'] as String,
+        createdAt: DateTime.parse(json['createdAt'] as String),
+        updatedAt: DateTime.parse(json['updatedAt'] as String),
+      );
+}
